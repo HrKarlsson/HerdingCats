18582f62310131371a850155f84daead6ae34ea2
Mario Roskosch <mario.roskosch@assfinet.de>
2016-09-04 11:57:43 +0200

    Init with gitignore

---
+++ OutlookPRJWeb/AppCompose/Home/Home.js
@@ -0,0 +1,41 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    'use strict';
+
+    // Die Initialisierungsfunktion muss bei jedem Laden einer neuen Seite ausgeführt werden.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            $('#set-subject').click(setSubject);
+            $('#get-subject').click(getSubject);
+            $('#add-to-recipients').click(addToRecipients);
+        });
+    };
+
+    function setSubject() {
+        Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.setAsync("Hello World!");
+    }
+
+    function getSubject() {
+        Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.getAsync(function (result) {
+            app.showNotification('Das aktuelle Thema ist', result.value)
+        });
+    }
+
+    function addToRecipients() {
+        var item = Office.context.mailbox.item;
+        var addressToAdd = {
+            displayName: Office.context.mailbox.userProfile.displayName,
+            emailAddress: Office.context.mailbox.userProfile.emailAddress
+        };
+ 
+        if (item.itemType === Office.MailboxEnums.ItemType.Message) {
+            Office.cast.item.toMessageCompose(item).to.addAsync([addressToAdd]);
+        } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
+            Office.cast.item.toAppointmentCompose(item).requiredAttendees.addAsync([addressToAdd]);
+        }
+    }
+
+})();
---
+++ OutlookPRJWeb/AppRead/Home/Home.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // Die Office-Initialisierungsfunktion muss bei jedem Laden einer neuen Seite ausgeführt werden.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            displayItemDetails();
+        });
+    };
+
+    // Zeigt die Felder "Betreff" und "Von" an, basierend auf dem aktuellen E-Mail-Element.
+    function displayItemDetails() {
+        var item = Office.cast.item.toItemRead(Office.context.mailbox.item);
+        $('#subject').text(item.subject);
+
+        var from;
+        if (item.itemType === Office.MailboxEnums.ItemType.Message) {
+            from = Office.cast.item.toMessageRead(item).from;
+        } else if (item.itemType === Office.MailboxEnums.ItemType.Appointment) {
+            from = Office.cast.item.toAppointmentRead(item).organizer;
+        }
+
+        if (from) {
+            $('#from').text(from.displayName);
+            $('#from').click(function () {
+                app.showNotification(from.displayName, from.emailAddress);
+            });
+        }
+    }
+})();
